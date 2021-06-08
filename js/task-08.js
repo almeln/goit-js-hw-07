7627b9d486114010b648c22468876356ae6e52da
Alina Melnyk <alinkaaa345@gmail.com>
2021-06-08 22:35:36 +0300

    update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -6,13 +6,31 @@ const buttonDestroy = document.querySelector('[data-action="destroy]');
 
 const boxesEl = document.querySelector('#boxes');
 
+function onInputChange(event) {
+    let amount = event.currentTarget.value;
+    console.log(amount);
+};
+
+counterEl.addEventListener('input', onInputChange);
+
 
 function createBoxes(amount) {
 
     for (let i = 0; i <= amount; i += 1) {
-        const divEl = document.createElement('div');
+        divEl = document.createElement('div');
         console.log(divEl);
     }
+
+    return divEl;
   };
 
-  buttonRender.addEventListener('click', createBoxes);
+buttonRender.addEventListener('click', createBoxes);
+
+
+
+
+
+
+
+
+
